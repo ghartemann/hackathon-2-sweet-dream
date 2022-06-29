dc282653f2df69eb5c9d35867ce0f0fe770e324c
EmmanuelleAllain <e.allain13@gmail.com>
2022-06-29 17:00:49 +0200

    project fixtures

--- src/DataFixtures/ProjectFixtures.php
+++ src/DataFixtures/ProjectFixtures.php
@@ -15,6 +15,29 @@ class ProjectFixtures extends Fixture
         for ($i = 0; $i < 10; $i++) 
         {
         $project = new Project();
+        $project->setTitle($faker->unique()->randomElement([
+                'Super projet',
+                'Webapp pour une association',
+                'Projet d\'appli géolocalisée',
+                'Un autre super projet',
+                'Projet de webapp',
+                'Projet secret',
+                'Site e-commerce',
+                'Projet génial',
+                'Nouveau projet',
+                'Appli de suivi médical'
+                ]))
+                ->setTopic($faker->randomElement(
+                    ['Banque', 'Assurance', 'Médecine', 'Aéronautique', 'Energie']
+                ))
+                ->setAgency($faker->randomElement(
+                    ['Lyon', 'Casablanca', 'Rennes', 'Tours', 'Strasbourg', 'Munich']
+                ))
+                ->setTechno($faker->randomElement(
+                    ['Javascript', 'Java', 'PHP']
+                ))
+                ->setDescription($faker->text(100))
+            ;
 
         $manager->persist($project);
 
