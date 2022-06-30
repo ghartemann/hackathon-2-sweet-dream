f6fdac1614b74de3f86e2d9d34492ed3fcdeb9d1
ghartemann <g.hartemann@gmail.com>
2022-06-30 14:25:16 +0200

    fixes

--- src/Form/ProjectType.php
+++ src/Form/ProjectType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Project;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -18,31 +19,33 @@ class ProjectType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title', TextType::class, ['label' => 'Titre'])
-            ->add('topic', TextType::class, ['label' => 'Sujet'])
-            ->add('agency', TextType::class, ['label' => 'Agence'])
+            ->add('title', TextType::class, ['label' => 'Title'])
+            ->add('topic', TextType::class, ['label' => 'Field'])
+            ->add('agency', TextType::class, ['label' => 'Office'])
             ->add('techno', ChoiceType::class, array(
-                    'choices' => 
+                'choices' =>
                     array
                     (
-                            'Javascript' => 'Javascript',
-                            'Java' => 'Java',
-                            'PHP' => 'PHP',
-                            'Vue.js' => 'Vue.js',
-                            'C#' => 'C#',
-                            'C++' => 'C++',
-                            'MySQL' => 'MySQL',
-                            'NextJS' => 'NextJS',
-                            'HTML' => 'HTML',
-                            'SCSS' => 'SCSS'
+                        'Javascript' => 'Javascript',
+                        'Java' => 'Java',
+                        'PHP' => 'PHP',
+                        'Vue.js' => 'Vue.js',
+                        'C#' => 'C#',
+                        'C++' => 'C++',
+                        'MySQL' => 'MySQL',
+                        'NextJS' => 'NextJS',
+                        'HTML' => 'HTML',
+                        'SCSS' => 'SCSS',
                     ),
-                    'multiple' => true,
-                    'expanded' => true,
-                    'required' => true
+                'multiple' => true,
+                'expanded' => true,
+                'required' => true,
             ))
-            ->add('description', TextType::class, ['label' => 'Description'])
-            ->add('meeting', TextType::class, ['label' => 'Réunion à venir'])
-        ;
+            ->add('description', TextareaType::class, ['label' => 'Description'])
+            ->add('meeting', TextType::class, ['label' => 'Meeting date', 'required' => false,
+                'attr' => array(
+                    'placeholder' => 'Leave empty if date is TBD',
+                )]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
