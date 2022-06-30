438a98ee4d980d761fa3f0215c216df5327d1822
dldvlpr <dormoy.dev@gmail.com>
2022-06-30 13:55:34 +0200

    ca fonctionne

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -6,10 +6,19 @@ use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
-
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
 {
+
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR');
@@ -21,7 +30,12 @@ class UserFixtures extends Fixture
             $user->setLastName($faker->lastName());
             $user->setPhone($faker->phoneNumber());
             $user->setEmail($faker->email());
-            $user->setPassword($faker->password());
+            $hashedPassword = $this->passwordHasher->hashPassword(
+                $user,
+                'password'
+            );
+            $user->setPassword($hashedPassword);
+
             $user->setAgency($faker->randomElement(
                 [
                     'Nantes',
